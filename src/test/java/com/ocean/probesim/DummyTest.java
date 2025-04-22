bfffd342d67045fe28cea8357a5b88a2f18957fd
Chandrakanta Behera <chandrakantab@maveric-systems.com>
2025-04-22 09:07:29 +0530

    initial project setup with test framework

---
+++ src/test/java/com/ocean/probesim/DummyTest.java
@@ -0,0 +1,13 @@
+package com.ocean.probesim;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DummyTest {
+
+    @Test
+    void shouldFailInitially() {
+        assertEquals(1, 2); // Intentional fail
+    }
+}
