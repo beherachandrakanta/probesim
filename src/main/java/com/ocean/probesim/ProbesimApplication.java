7ab91e43a7850ffef26750bfa7300540f80094f8
Chandrakanta Behera <chandrakantab@maveric-systems.com>
2025-04-22 08:27:19 +0530

    Initial commit project setup

---
+++ src/main/java/com/ocean/probesim/ProbesimApplication.java
@@ -0,0 +1,13 @@
+package com.ocean.probesim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbesimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbesimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ocean/probesim/ProbesimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocean.probesim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbesimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
