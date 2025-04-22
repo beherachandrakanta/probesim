021c46da3c73dc7c2b80998a18df4f948eec35b2
Chandrakanta Behera <chandrakantab@maveric-systems.com>
2025-04-22 15:18:25 +0530

    implement probe service logic to test

--- src/test/java/com/ocean/probesim/controller/ProbeControllerTest.java
+++ src/test/java/com/ocean/probesim/controller/ProbeControllerTest.java
@@ -3,9 +3,15 @@ package com.ocean.probesim.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ocean.probesim.dto.CommandRequest;
 import com.ocean.probesim.dto.InitRequest;
+import com.ocean.probesim.service.ProbeService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,11 +20,21 @@ import java.util.List;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(ProbeController.class)
+@AutoConfigureMockMvc
+@WebMvcTest(controllers = ProbeController.class)
 class ProbeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public ProbeService probeService() {
+            return Mockito.mock(ProbeService.class);
+        }
+    }
+
 
     @Autowired
     private ObjectMapper objectMapper;
